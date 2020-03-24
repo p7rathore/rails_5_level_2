b2d1584774430b3e1f9063203df300ed28fd9cca
P_7_Rathore <rathore@example.com>
2020-03-24 14:11:02 +0530

    create_model_and_migration(Event Model)

--- spec/features/list_events_spec.rb
+++ spec/features/list_events_spec.rb
@@ -2,11 +2,42 @@ require "rails_helper"
 
 describe "Viewing the list of events" do 
 	it "shows the event" do 
-		# visit "http://example.com/events"
-		visit events_url #we also write evets_path
+		event1 = Event.create(name: "BugSmash",
+							  location: "Danver",
+							  price: 10.00,
+							  description: "A fun evening of bug smashing",
+							  starts_at: 10.days.from_now)
+
+		event2 = Event.create(name: "Hackathon",
+							  location: "Austin",
+							  price: 15.00,
+							  description: "Hunker down at the Hackathon",
+							  starts_at: 15.days.from_now)
+
+		event3 = Event.create(name: "kata Camp",
+							  location: "Dallas",
+							  price: 75.00,
+							  description: 'pratice your craft kata style',
+							  starts_at: 30.days.from_now)
+
+		visit events_url 
 		expect(page).to have_text("3 Events")
-		expect(page).to have_text("Event 1")
-		expect(page).to have_text("Event 2")
-		expect(page).to have_text("Event 3")
+		expect(page).to have_text(event1.name)
+		expect(page).to have_text(event1.location)
+		expect(page).to have_text(event1.description[1..10])
+		expect(page).to have_text(event1.starts_at)
+		expect(page).to have_text(event1.price)
+		
+		expect(page).to have_text(event2.name)
+		expect(page).to have_text(event2.location)
+		expect(page).to have_text(event2.description[1..10])
+		expect(page).to have_text(event2.starts_at)
+		expect(page).to have_text(event2.price)
+		
+		expect(page).to have_text(event3.name)
+		expect(page).to have_text(event3.location)
+		expect(page).to have_text(event3.description[1..10])
+		expect(page).to have_text(event3.starts_at)
+		expect(page).to have_text(event3.price)
 	end	
 end
