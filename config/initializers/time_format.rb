61de477788b5a98d90258e2fe7f6c330501ee782
P_7_Rathore <rathore@example.com>
2020-03-24 18:40:11 +0530

    create_browser_view_of_application(Check in browser)

---
+++ config/initializers/time_format.rb
@@ -0,0 +1,2 @@
+Time::DATE_FORMATS[:default]= "%B %d, %Y at %I:%M %p"
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Event.create!([
+	{
+		name: "BugSmash",
+		location: "Danver, CO",
+		price: 0.00,
+		description: "A fun evening of bug smashing",
+		starts_at: 100.days.from_now
+	},
+	{
+		name: "Hackathon",
+		location: "Austin, TX",
+		price: 15.00,
+		description: "Hunker down at the Hackathon",
+		starts_at: 15.days.from_now
+	},
+	{
+		name: "kata Camp",
+		location: "Dallas, TX",
+		price: 75.00,
+		description: "pratice your craft kata style",
+		starts_at: 30.days.from_now
+	}
+])
