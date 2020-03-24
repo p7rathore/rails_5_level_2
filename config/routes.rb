7f7664f69ef4a86f659ea71d55eb2c3b713a9930
P_7_Rathore <rathore@example.com>
2020-03-24 19:59:30 +0530

    create_edit_url_and_file(Form Editing 1)

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
 	root "events#index"
 	get "events" => "events#index"
 	get "events/:id" => "events#show", as: "event"
+	get "events/:id/edit" => "events#edit", as: "edit_event"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
