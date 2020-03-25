35579e672786c7e3e672da2d5b5dae624095d08b
P_7_Rathore <rathore@example.com>
2020-03-25 13:01:21 +0530

    create_new_event(Forms: Creating)

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -11,10 +11,27 @@ class EventsController < ApplicationController
 		@event = Event.find(params[:id])
 	end
 
+
 	def update
 		@event = Event.find(params[:id])
-		event_params = params.require(:event).permit(:name, :description, :location, :price, :starts_at) 
 		@event.update(event_params)
-		redirect_to @event #we also write redirect_to event_path(@event)
+		redirect_to @event #we also write redirect_to event_path(@event) its a shoe page url
 	end
+
+	def new
+		@event = Event.new
+	end
+
+	def create
+		# @event = Event.create(event_params) # when use it then we dont require write @event.save
+		@event = Event.new(event_params)# but when use it then we  require write @event.save
+		@event.save
+		redirect_to @event #we also write redirect_to event_path(@event) its a show page url
+
+	end
+
+	private 
+		def event_params
+			params.require(:event).permit(:name, :ldescription, :location, :price, :starts_at)
+		end 
 end
