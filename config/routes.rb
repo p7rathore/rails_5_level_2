69f5409c31436749fa2fcc18be12b648650f45ec
P_7_Rathore <rathore@example.com>
2020-03-25 11:27:17 +0530

    updating_events(Forms: Editng_2)

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 	get "events" => "events#index"
 	get "events/:id" => "events#show", as: "event"
 	get "events/:id/edit" => "events#edit", as: "edit_event"
+	patch "events/:id" => "events#update" # never need to create helper method b/c we reach on show page when click update button so use show helper method
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
