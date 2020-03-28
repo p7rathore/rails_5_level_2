7a9a5b1131b0408a861eccacea724975daaf0ee3
P_7_Rathore <rathore@example.com>
2020-03-28 11:49:53 +0530

    create_Admin_user_and_create_require_admin_mthod_in_application_controller(Admin User)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,12 @@ class ApplicationController < ActionController::Base
 		 end
 		end
 
+		def require_admin
+			unless current_user.admin?
+				redirect_to root_url, notice: "unauthorized user!"
+			end
+		end
+
 		def current_user
 			User.find(session[:user_id]) if session[:user_id]
 		end
@@ -19,4 +25,10 @@ class ApplicationController < ActionController::Base
 		end
 
 		helper_method :current_user?
+
+		def current_user_admin?
+			current_user && current_user.admin?
+		end
+
+		helper_method :current_user_admin?
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_27_074359) do
+ActiveRecord::Schema.define(version: 2020_03_28_055830) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2020_03_27_074359) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false
   end
 
   add_foreign_key "registrations", "events"
