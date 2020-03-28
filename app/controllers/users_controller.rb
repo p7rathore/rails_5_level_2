6f15cf3ddf515e585894a30bc52062656640e4dc
P_7_Rathore <rathore@example.com>
2020-03-28 11:03:24 +0530

    store_url_into_session_and_create_current_user?_helper_method_in_application_controller(Authorization:part-2)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 	before_action :require_signin, except: [:new,:create] 
+	before_action :require_correct_user, only: [:edit, :update, :destroy]
+
 	def index
 		@users = User.all 
 	end
@@ -23,11 +25,11 @@ class UsersController < ApplicationController
 	end
 
 	def edit
-		@user = User.find(params[:id])
+		# @user = User.find(params[:id])
 	end
 
 	def update
-		@user = User.find(params[:id])
+		# @user = User.find(params[:id])
 		if @user.update(user_params)
 			redirect_to user_path(@user), notice: "Account successfully updated!"
 		else
@@ -36,14 +38,24 @@ class UsersController < ApplicationController
 	end
 
 	def destroy
-		@user = User.find(params[:id])
+		# @user = User.find(params[:id])
 		@user.destroy
 		session[:user_id] = nil
 		redirect_to users_path, notice: "Your Account successfully Deleted!"
 	end
 
 	private
+
+		def require_correct_user
+			@user = User.find(params[:id])
+			unless current_user?(@user)
+				redirect_to root_url
+			end
+
+		end
 		def user_params
 			params.require(:user).permit(:name, :email, :password, :password_confirmation)
 		end
+
+
 end
